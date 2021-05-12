217aecd59606289a9067ebd3abd03dc6afeef0b3
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2021-05-11 21:57:03 -0700

    Introduce python-control module for differential flatness experiments

--- diff_drive_robot.py
+++ diff_drive_robot.py
@@ -1,10 +1,67 @@
+import enum
+import time
+
 import numpy as np
 import scipy.integrate
+import control.flatsys as flatsys
 
 from PyQt5 import QtGui, QtCore
 
+class DifferentialDriveRobotFlatSystem(flatsys.FlatSystem):
+    def __init__(self, r, L):
+        self.r = r # wheel radius
+        self.L = L # baseline
+        super(DifferentialDriveRobotFlatSystem, self).__init__(self.forward,
+                                                               self.reverse,
+                                                               params={'r': r, 'L': L},
+                                                               inputs=['ω_l', 'ω_r'],
+                                                               outputs=['flat_x', 'flat_y'],
+                                                               states=['x', 'y', 'θ'])
+
+    # /__init__()
+
+    def forward(self, s, u):
+        x, y, θ = s
+        ω_l, ω_r = u
+        v = self.r * 0.5 * (ω_l + ω_r)
+        x_dot = v * np.cos(θ)
+        y_dot = v * np.sin(θ)
+        θ_dot = (self.r / self.L) * (ω_r - ω_l)
+        x_ddot = -y_dot * θ_dot
+        y_ddot = x_dot * θ_dot
+        # 'control' package calls this the 'flag'
+        flat_flag = [np.array([x, x_dot, x_ddot]),
+                     np.array([y, y_dot, y_ddot])]
+        return flat_flag
+    # /flatsysForward()
+
+    def reverse(self, flat_flag):
+        x, x_dot, x_ddot = flat_flag[0]
+        y, y_dot, y_ddot = flat_flag[1]
+        θ = np.arctan2(y_dot, x_dot)
+        v_sqr = (x_dot * x_dot) + (y_dot * y_dot)
+        v = np.sqrt(v_sqr)
+        ωr_plus_ωl = (2/self.r) * v
+        ωr_minus_ωl = (self.L/self.r) * (x_dot * y_ddot - x_ddot * y_dot) / v_sqr
+        ωr = 0.5 * (ωr_plus_ωl + ωr_minus_ωl)
+        ωl = ωr_plus_ωl - ωr
+        s = np.array([x, y, θ])
+        u = np.array([ωl, ωr])
+        return s, u
+    # /flatsysReverse()
+
+# /class DifferentialDriveRobotFlatSystem
+class FsmState(enum.IntEnum):
+    IDLE = 0
+    PLANNING = 1
+    DRIVING = 2
+# /class State
+
+
 class DifferentialDriveRobot:
+    
     def __init__(self, radius, wheel_radius, wheel_thickness):
+        self.fsm_state = FsmState.IDLE
         self.radius = radius
         self.wheel_radius = wheel_radius
         self.wheel_thickness = wheel_thickness
@@ -12,39 +69,99 @@ class DifferentialDriveRobot:
         x = self.radius
         y = self.radius + self.wheel_thickness
         theta = 0
-        self.state = np.array([x, y, theta])
+        self.s = np.array([x, y, theta])
+        self.t = 0 # relative time within a driving mission
+
+        self.flatsys = DifferentialDriveRobotFlatSystem(self.wheel_radius, 2*self.radius)
+
+        # self.flatsys = flatsys.FlatSystem(self.flatsysForward,
+        #                                   self.flatsysReverse,
+        #                                   updfcn=self.dynamics,
+        #                                   params={'r': self.wheel_radius, 'L': 2*self.radius},
+        #                                   inputs=['omega_l', 'omega_r'],
+        #                                   outputs=['flat_x', 'flat_y'],
+        #                                   states=['x', 'y', 'theta'])
     # /__init__()
 
-    def dynamics(self, x_y_theta, t, omega_l, omega_r, wheel_radius, baseline):
-        _, _, theta = x_y_theta
-        v = wheel_radius * 0.5 * (omega_l + omega_r)
-        x_dot = v * np.cos(theta)
-        y_dot = v * np.sin(theta)
-        theta_dot = wheel_radius / baseline * (omega_r - omega_l)
-        return np.array([x_dot, y_dot, theta_dot])
+    def fsmTransition(self, fsm_state):
+        print('State transition: ', self.fsm_state, '->', fsm_state)
+        self.fsm_state = fsm_state
+    # fsmTransition()
+
+    def dynamics(self, t, s, u, params):
+        _, _, θ = s
+        ω_l, ω_r = u
+        r = params['r'] # wheel radius
+        L = params['L'] # baseline
+        v = (r/2) * (ω_r + ω_l)
+        x_dot = v * np.cos(θ)
+        y_dot = v * np.sin(θ)
+        θ_dot = (r/L) * (ω_r - ω_l)
+        return np.array([x_dot, y_dot, θ_dot])
     # /dynamics()
 
-    def applyControl(self, omega_l, omega_r, delta_t):
+    def applyControl(self, delta_t, u):
         '''
-            omega_{l,r}: angular velocities of left and right wheels in rad/s
+            u: angular velocities of left and right wheels, respectively, in rad/s
         '''
-        self.state = scipy.integrate.odeint(self.dynamics,
-                                            self.state,
-                                            np.array([0, delta_t]),
-                                            (omega_l, omega_r, self.wheel_radius, 2 * self.radius))[1]
+        self.s = scipy.integrate.odeint(self.dynamics,
+                                        self.s,
+                                        np.array([0, delta_t]),
+                                        args=(u, {'r' : self.wheel_radius, 'L' : 2 * self.radius}),
+                                        tfirst=True)[1]
     # /applyControls()
+
+    def goto(self, target_state, duration):
+        self.fsmTransition(FsmState.PLANNING)
+        N = int(duration / 0.1)
+        self.timepts = np.linspace(0, duration, N)
+        traj_func = flatsys.point_to_point(self.flatsys, self.timepts, x0 = self.s, xf=target_state)
+        self.s, _ = traj_func.eval(self.timepts)
+        # print('s.shape =', self.s.shape)
+        self.s = self.s.T
+        self.t_drive_begin = time.time()
+        self.s_counter = 0
+        self.fsmTransition(FsmState.DRIVING)
+    # /goto()
 
     # Draw this instance onto a qpainter
     def render(self, qpainter, window_height):
-        x, y, theta = self.state
-        # Position
+        if self.fsm_state == FsmState.PLANNING:
+            return
+        # /if
+
+        t_drive = time.time() - self.t_drive_begin
+
+        if self.fsm_state == FsmState.DRIVING:
+            while self.s_counter < len(self.s) and self.timepts[self.s_counter] < t_drive:
+                self.s_counter += 1
+            #/
+            if self.s_counter == len(self.timepts):
+                self.fsmTransition(FsmState.IDLE)
+            # /if
+
+            self.s_counter -= 1
+        # /if
+
+        x, y, θ = self.s[self.s_counter]
         original_transform = qpainter.worldTransform()
         transform = QtGui.QTransform()
         transform.translate(x, window_height -1 -y)
-        transform.rotate(-theta * 180 / np.pi) # degrees
+        transform.rotate(-θ * 180 / np.pi) # degrees
         qpainter.setWorldTransform(transform, combine=False)
 
-        # main frame
+        self.renderCanonical(qpainter, window_height)
+
+        qpainter.setWorldTransform(original_transform)
+        
+    # /render()
+
+    def renderCanonical(self, qpainter, window_height):
+        '''
+            Renders the robot in canonical coordinate frame.
+            Call after setting the world transformation.
+        '''
+        # main body
         brush = QtGui.QBrush()
         brush.setColor(QtCore.Qt.black)
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -53,9 +170,10 @@ class DifferentialDriveRobot:
         qpainter.drawRect(-self.wheel_radius, -self.radius -self.wheel_thickness, 2 * self.wheel_radius, self.wheel_thickness)
         qpainter.drawRect(-self.wheel_radius,  self.radius,                       2 * self.wheel_radius, self.wheel_thickness)
 
-        # draw single dot to mark orientation
+        # single dot to mark orientation
         brush.setColor(QtCore.Qt.yellow)
         qpainter.setBrush(brush)
         qpainter.drawEllipse(QtCore.QPoint(0.75 * self.radius, 0), 0.1 * self.radius, 0.1 * self.radius)
-        qpainter.setWorldTransform(original_transform)
-    # /render()
+    # /renderCanonical()
+
+# /class DifferentialDriveRobot
--- main.py
+++ main.py
@@ -13,35 +13,38 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.label)
 
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.animate)
+        self.timer.timeout.connect(self.render)
         self.counter = 0
 
-        t = np.arange(0, 5, 0.1)
-        omega_l = 3.0
-        omega_r = 4.0
-        self.states = np.empty((len(t), 3))
-        self.states[0] = diff_dr_robot.state
-        for i in range(1,len(t)):
-            dt = t[i] - t[i-1]
-            if t[i] > 2.5:
-                omega_l, omega_r = omega_r, omega_l
-            diff_dr_robot.applyControl(omega_l, omega_r, dt)
-            self.states[i] = diff_dr_robot.state
-        # /for i
-        
+        if False:
+            t = np.arange(0, 5, 0.1)
+            omega_l = 3.0
+            omega_r = 4.0
+            self.states = np.empty((len(t), 3))
+            self.states[0] = diff_dr_robot.state
+            for i in range(1,len(t)):
+                dt = t[i] - t[i-1]
+                if t[i] > 2.5:
+                    omega_l, omega_r = omega_r, omega_l
+                u = np.array([omega_l, omega_r])
+                diff_dr_robot.applyControl(dt, u)
+                self.states[i] = diff_dr_robot.state
+            # /for i
+        # /if
+
         self.diff_dr_robot = diff_dr_robot
-        self.timer.start(int(dt * 1000))
+        self.timer.start(100) # ms
 
     # /__init__()
 
-    def animate(self):
-        if self.counter < self.states.shape[0]:
-            self.diff_dr_robot.state = self.states[self.counter]
-            self.render()
-            # print('Rendered state ', self.counter, ': ', self.states[self.counter])
-            self.counter += 1
-            self.update()
-    # /animate()
+    # def animate(self):
+    #     if self.counter < self.states.shape[0]:
+    #         self.diff_dr_robot.state = self.states[self.counter]
+    #         self.render()
+    #         # print('Rendered state ', self.counter, ': ', self.states[self.counter])
+    #         self.counter += 1
+    #         self.update()
+    # # /animate()
 
     def render(self):
         qpainter = QtGui.QPainter(self.label.pixmap())
@@ -57,6 +60,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.diff_dr_robot.render(qpainter, self.label.height())
 
         qpainter.end()
+        self.update()
     # /render()
 
 # /class MainWindow
@@ -67,4 +71,5 @@ diff_dr_robot = diff_drive_robot.DifferentialDriveRobot(radius=50,
                                                         wheel_thickness=10)
 window = MainWindow(600, 400, diff_dr_robot)
 window.show()
+diff_dr_robot.goto(np.array([300, 200, np.pi/180 * 90]), 5.0)
 app.exec_()
